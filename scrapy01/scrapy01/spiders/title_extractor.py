7691c865a0a5f69e9b7689c217f92fd3a39f1fda
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-03-18 13:19:38 +0330

    Completion of title_extractor spider.

--- scrapy01/scrapy01/spiders/title_extractor.py
+++ scrapy01/scrapy01/spiders/title_extractor.py
@@ -1,11 +1,15 @@
 import scrapy
 
 
-class ExampleSpider(scrapy.Spider):
-    name = "example"
-    # allowed_domains = ["example.com"]
+class TitleExtractorSpider(scrapy.Spider):
+    """
+    Extracting the title of the movie.
+    """
+    name = "title_extractor"
+    allowed_domains = ["imdb.com"]
     start_urls = ["https://www.imdb.com/title/tt1375666/"]
     custom_settings = {
+        # Defining a fake User_Agent
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
     }
 
