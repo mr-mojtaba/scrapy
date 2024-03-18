3167c07390b15c25248d2abb9344fb18868a23cf
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-03-18 17:35:48 +0330

    Adding the USER_AGENT

--- scrapy01/scrapy01/settings.py
+++ scrapy01/scrapy01/settings.py
@@ -91,3 +91,5 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+# USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
