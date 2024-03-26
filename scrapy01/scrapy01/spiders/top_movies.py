0a01d02ba1ab27dce74939e9c25802d4708f0a65
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2024-03-26 20:34:23 +0330

    Adding some items scraping.

--- scrapy01/scrapy01/spiders/top_movies.py
+++ scrapy01/scrapy01/spiders/top_movies.py
@@ -27,11 +27,36 @@ class TopMoviesSpider(CrawlSpider):
     i = 0
 
     def parse_items(self, response):
+        name_item = response.css('h1 span::text').extract_first()
+        year_item = response.css(
+                'ul.ipc-inline-list > li.ipc-inline-list__item > a.ipc-link--baseAlt[href*="releaseinfo"]::text').get()
+        rating_item = response.css("span.sc-bde20123-1::text").get()
+        vote = response.css("div.sc-bde20123-3::text").get()
+        genre_item = response.xpath("//div[@class='ipc-chip-list__scroller']//a//text()").getall()
+        duration_item = response.css('ul.ipc-inline-list > li.ipc-inline-list__item:nth-child(3)::text').get()
+        director_item = response.css("a.ipc-metadata-list-item__list-content-item::text").get()
+        writer_item = set(
+            response.css(
+                'ul.ipc-metadata-list li:nth-child(2).ipc-metadata-list__item > '
+                'div.ipc-metadata-list-item__content-container > ul.ipc-inline-list > li.ipc-inline-list__item > '
+                'a.ipc-metadata-list-item__list-content-item[href*="/name/nm"]::text').getall())
+        stars_item = set(response.xpath("//a[text()='Stars']/following-sibling::div//a//text()").getall())
+        synopsis = response.css('span.sc-466bb6c-1::text').get()
+        link_item = response.url
+
         self.i += 1
         print("\t")
         print("*" * 20)
         print(self.i)
-        print("Movie name: ", response.css('h1 span::text').extract_first())
-        print("Link: ", response.url)
+        print("Movie name: {}".format(name_item))
+        print("Date of Release: {}".format(year_item))
+        print("IMDB Rating: {}/10 - {} Vote".format(rating_item, vote))
+        print("Genre: {}".format(", ".join(str(item) for item in genre_item)))
+        print("Duration: {}".format(duration_item))
+        print("Director: {}".format(director_item))
+        print("Writers: {}".format(", ".join(str(item) for item in writer_item)))
+        print("Stars: {}".format(", ".join(str(item) for item in stars_item)))
+        print("Synopsis : {}".format(synopsis))
+        print("Link: {}".format(link_item))
         print("*" * 20)
         print("\t")
